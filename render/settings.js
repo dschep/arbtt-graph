be884d879f6de32c585c8b372a327900f5e92dcf
Daniel Schep <dschep@gmail.com>
2016-06-03 22:06:18 -0400

    Personal updates

--- render/settings.js
+++ render/settings.js
@@ -1,8 +1,21 @@
 // Barcode view settings
 var display_groups = [];
-// Productive
-display_groups.push(["read-djvu", "read-pdf", "read-epub", "read-mobi", "code-web", "code-python", "code-haskell", "code-ruby", "code-matlab", "write", "term", "mail"]);
-// Unproductive
-display_groups.push(["irc", "social", "communities", "tv-local", "tv-web", "read-rss"]);
-// Don't know
-display_groups.push(["misc", "browsing"]);
+// Coding
+display_groups.push([
+        "PersonalCoding",
+        "ThredeCoding",
+        "EndgameCoding",
+]);
+// Time wasters
+display_groups.push([
+        "PersonalBrowsing",
+        "Social",
+]);
+// Communication
+display_groups.push([
+        "EndgameChat",
+        "PersonalEmail",
+        "ThredeChat",
+        "ThredeEmail",
+        "EndgameEmail",
+]);
